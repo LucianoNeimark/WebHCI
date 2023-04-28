2f85159df51c8143e52f31bb58022d47170d065e
davidwis <davidwis000@gmail.com>
2023-04-28 20:23:13 -0300

    Created Oven Card

--- src/components/device-cards/OvenCard.vue
+++ src/components/device-cards/OvenCard.vue
@@ -1,14 +1,42 @@
 <script setup>
 import FrameCard from "@/components/FrameCard.vue"
 import OvenSVG from "@/assets/device-icons/device/oven.svg"
+import {computed, ref} from "vue";
 defineProps({
     id: String,
     name : String
 })
 
+const temp = ref(90)
+const power = ref(false)
+
+const powerButtonColor = computed(() => {
+    return power.value ? 'white' : 'green' // TODO: Change to constants
+})
+
 </script>
 <template>
     <FrameCard :name="name" :icon="OvenSVG">
+        <VContainer>
+            <VRow class="flex-row justify-center">
+
+                <VBtn rounded="xl" @click="power = !power" :color="powerButtonColor" stacked>
+                    <VIcon icon="mdi:mdi-power"></VIcon>
+                </VBtn>
+            </VRow>
+            <VRow>
+                <VSlider
+                    v-model="temp"
+                    :min="90"
+                    :max="240"
+                    step="1"
+                    thumb-label
+                    track-fill-color="white"
+                    thumb-color="white"
+                    append-icon="mdi-thermometer-high"
+                ></VSlider>
+            </VRow>
+        </VContainer>
     </FrameCard>
 </template>
 
