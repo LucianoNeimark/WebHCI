4589ed355c248da771e908b7f0ff31b6286bd3a2
davidwis <davidwis000@gmail.com>
2023-04-28 19:22:13 -0300

    Added material icons package

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 
 import App from './App.vue'
 import router from './router'
