ce64a51e33a13049555aa20af38722a54c4a5d30
“avilamowski” <abruvila@gmail.com>
2023-04-29 11:34:37 -0300

    Created light card

--- src/components/device-cards/LightCard.vue
+++ src/components/device-cards/LightCard.vue
@@ -1,14 +1,30 @@
 <script setup>
 import FrameCard from "@/components/FrameCard.vue"
 import LightbulbSVG from "@/assets/device-icons/device/lightbulb.svg"
+import {computed, ref} from "vue";
+
 defineProps({
     id: String,
     name : String
 })
 
+const power = ref(false)
+
+const powerButtonColor = computed(() => {
+    return power.value ? 'green' : 'white' // TODO: Change to constants
+})
+
 </script>
+
 <template>
     <FrameCard :name="name" :icon="LightbulbSVG">
+        <VContainer>
+            <VRow class="flex-row justify-center mb-1">
+                <VBtn rounded="xl" @click="power = !power" :color="powerButtonColor" size="x-large" stacked>
+                    <VIcon icon="mdi:mdi-power" size="x-large"></VIcon>
+                </VBtn>
+            </VRow>
+        </VContainer>
     </FrameCard>
 </template>
 
