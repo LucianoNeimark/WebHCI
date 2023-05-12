1dd5b5221d0f731d5caa0ea7a4c7bbcdfd9de876
avilamowski <abruvila@gmail.com>
2023-05-12 18:23:38 -0300

    starting blind2

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -12,6 +12,8 @@ import DoorCard from "@/components/cards/device-cards/DoorCard.vue";
 import DoorInfo from "@/components/device-pages/DoorInfo.vue";
 import RefrigeratorCard from "@/components/cards/device-cards/RefrigeratorCard.vue";
 import RefrigeratorInfo from "@/components/device-pages/RefrigeratorInfo.vue";
+import BlindCard from "@/components/cards/device-cards/BlindCard.vue";
+import BlindInfo from "@/components/device-pages/BlindInfo.vue";
 
 export const CONSTANTS = {
   NO_ROOM: 'no_room',
@@ -30,7 +32,7 @@ export const deviceTypes = {
   'li6cbv5sdlatti0j': { name: 'AC', icon: 'mdi-air-conditioner', card: AirConditionerCard, info: AirConditionerInfo},
   'go46xmbqeomjrsjr': { name: 'Lámpara', icon: 'mdi-lightbulb-outline', card: LightCard, info: LightInfo},
   'c89b94e8581855bc': { name: 'Parlante', icon: 'mdi-volume-high' , card: SpeakerCard, info: SpeakerInfo},
-  'eu0v2xgprrhhg41g': { name: 'Persiana', icon: "mdi-blinds-horizontal", card: null, info: null},
+  'eu0v2xgprrhhg41g': { name: 'Persiana', icon: "mdi-blinds-horizontal", card: BlindCard, info: BlindInfo},
   'mxztsyjzsrq7iaqc': { name: 'Alarma', icon: "mdi-alarm-light-outline", card: AlarmCard, info: AlarmInfo},
   'im77xxyulpegfmv8': { name: 'Horno', icon: "mdi-toaster-oven", card: OvenCard, info: OvenInfo},
   'lsf78ly0eqrjbz91': { name: 'Puerta', icon: 'mdi-door-open', card: DoorCard, info: DoorInfo},
