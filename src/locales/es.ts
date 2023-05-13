cbd62cb20a0ea6c4620f2735971cc912c8d068d0
avilamowski <abruvila@gmail.com>
2023-05-12 23:36:54 -0300

    translations added to es

--- src/locales/es.ts
+++ src/locales/es.ts
@@ -3,10 +3,87 @@ import es from 'vuetify/lib/locale/es';
 
 export default {
     $vuetify: { ...es },
+    //Generos
     country: 'country',
     classical: 'clásica',
     dance: 'baile',
     latina: 'latina',
     pop: 'pop',
     rock: 'rock',
+
+    //Parlante
+    setVolume: 'establecer volumen',
+    play: 'reproducir',
+    pause: 'pausar',
+    stop: 'detener',
+    resume: 'reanudar',
+    nextSong: 'siguiente',
+    previousSong: 'anterior',
+    setGenre: 'establecer género',
+    getPlaylist: 'obtener lista de reproducción',
+
+    //Persiana
+    open: 'abrir',
+    close: 'cerrar',
+    setLevel: 'establecer nivel',
+
+    //Lampara
+    turnOn: 'encender',
+    turnOff: 'apagar',
+    setColor: 'establecer color',
+    setBrightness: 'establecer brillo',
+
+    //Horno
+    // turnOn: 'encender',
+    // turnOff: 'apagar',
+    setTemperature: 'establecer temperatura',
+    setHeat: 'establecer calor',
+    setGrill: 'establecer parrilla',
+    setConvection: 'establecer convección',
+
+    //Parametros
+    conventional: 'convencional',
+    bottom: 'inferior',
+    top: 'superior',
+    large: 'grande',
+    eco: 'eco',
+    off: 'apagado',
+    normal: 'normal',
+
+    //Aire acondicionado
+    // turnOn: 'encender',
+    // turnOff: 'apagar',
+    // setTemperature: 'establecer temperatura',
+    setMode: 'establecer modo',
+    setVerticalSwing: 'establecer oscilación vertical',
+    setHorizontalSwing: 'establecer oscilación horizontal',
+    setFanSpeed: 'establecer velocidad del ventilador',
+   
+    //Parametros
+    cool: 'frío',
+    heat: 'calor',
+    fan: 'ventilador',
+
+    //Puerta
+    // open: 'abrir',
+    // close: 'cerrar',
+    lock: 'bloquear',
+    unlock: 'desbloquear',
+
+    //Alarma
+    changeSecurityCode: 'cambiar código de seguridad',
+    armStay: 'activar alarma en casa',
+    armAway: 'activar alarma fuera de casa',
+    disarm: 'desactivar alarma',
+
+    //Heladera
+    setFreezerTemperature: 'establecer temperatura del freezer',
+    // setTemperature: 'establecer temperatura',
+    // setMode: 'establecer modo',
+
+    //Parametros
+    default: 'por defecto',
+    vacation: 'vacaciones',
+    party: 'fiesta',
+
 }
