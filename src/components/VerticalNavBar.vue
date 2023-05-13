1f930aac66a2f82173b9869143035b4681280dfc
avilamowski <abruvila@gmail.com>
2023-05-13 01:57:16 -0300

    logo and icon done

--- src/components/VerticalNavBar.vue
+++ src/components/VerticalNavBar.vue
@@ -1,20 +1,20 @@
 <script setup>
-
+import Logo from '@/assets/homewizard.png'
 </script>
 
 <template>
-
       <VNavigationDrawer
               app
               clipped
               color="primary"
               width=240
       >
+        <img class="align-center" :src="Logo" alt="logo" width="200"/>
           <VList color="transparent">
-              <VListItem prepend-icon="mdi-view-dashboard" title="Inicio" value="main" to="/"></VListItem>
-              <VListItem prepend-icon="mdi-lamp" title="Dispositivos" value="devices" to="/devices"></VListItem>
-              <VListItem prepend-icon="mdi-bed-king-outline" title="Habitaciones" value="rooms" to="/rooms"></VListItem>
-              <VListItem prepend-icon="mdi-clock-fast" title="Rutinas" value="routines"  to="/routines"></VListItem>
+            <VListItem prepend-icon="mdi-view-dashboard" title="Inicio" value="main" to="/"/>
+            <VListItem prepend-icon="mdi-lamp" title="Dispositivos" value="devices" to="/devices"/>
+            <VListItem prepend-icon="mdi-bed-king-outline" title="Habitaciones" value="rooms" to="/rooms"/>
+            <VListItem prepend-icon="mdi-clock-fast" title="Rutinas" value="routines"  to="/routines"/>
           </VList>
       </VNavigationDrawer>
 </template>
@@ -22,6 +22,14 @@
 
 <style lang="scss">
   @import '@/assets/variables.scss';
+
+  .align-center {
+      display: block;
+      margin-top: 3%;
+      margin-bottom: 2%;
+      margin-left: auto;
+      margin-right: auto;
+  }
 
   .v-list .v-list-item--active {
       background-color: $secondary !important;
